d153d31bf25f35c3195c176b436720990f0f8c37
patrick.doumont <patrick.doumont@outlook.com>
2020-09-24 12:59:31 -0400

    Added comments for clarity.

--- mpg_prediction.R
+++ mpg_prediction.R
@@ -1,14 +1,18 @@
 library("ggplot2")
+#Import a data set provided by R.
 data(mtcars)
 head(mtcars)
-
+#Create formula
 fmla <- mpg ~ cyl + hp + wt
 
+#Model the formula using linear regression
 mpg_formula <- lm(fmla, data = mtcars)
 
+#View the formula
 mpg_formula
 summary(mpg_formula)
 
+#Predict outcomes based on linear regression
 mpg_prediction <- predict(mpg_formula)
 
 # plot the results
